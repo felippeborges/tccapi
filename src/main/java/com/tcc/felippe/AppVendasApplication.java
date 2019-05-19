0a204b6f91acaba61b4da492a01c41334454371c
Felippe Borges <felippe-b@hotmail.com>
2019-05-19 13:42:35 -0300

    Iniciando projeto de TCC aplicativo de vendas com API

---
+++ src/main/java/com/tcc/felippe/AppVendasApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.felippe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVendasApplication.class, args);
+	}
+
+}
